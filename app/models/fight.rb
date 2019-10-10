8cdc45cb15ee8ee4a2645f4f5efc2cabb5ae9ef5
Lucas Hudson <lucashudson.contact@gmail.com>
2019-10-10 15:48:14 +0200

    added validation in models

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,4 +1,7 @@
 class Fight < ApplicationRecord
+  validates :loser, presence: true
+  validates :winner, presence: true
+
   belongs_to :loser, class_name: "Fighter", foreign_key: "loser_id"
   belongs_to :winner, class_name: "Fighter", foreign_key: "winner_id"
 end
--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,6 +1,10 @@
 class Fighter < ApplicationRecord
-  has_many :lost_fights, class_name: "Fight", foreign_key: "loser_id"
-  has_many :won_fights, class_name: "Fight", foreign_key: "winner_id"
+  validates :name, presence: true
+  validates :lifepoints, presence: true
+  validates :hitpoints, presence: true
+
+  has_many :defeats, class_name: "Fight", foreign_key: "loser_id"
+  has_many :victories, class_name: "Fight", foreign_key: "winner_id"
 
   def fights
     Fight.where(loser: self).or(Fight.where(winner: self))
