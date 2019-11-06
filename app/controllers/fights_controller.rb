f74e329d6af165328c698bb9a0b71e9a19321ebe
Lucas Hudson <lucashudson.contact@gmail.com>
2019-11-06 18:00:37 +0100

    fixed class issues in FightSequence

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -10,17 +10,11 @@ class FightsController < ApplicationController
   end
 
   def create
-    @fighters = Fighter.all
-    @fight = Fight.new
-    @accessories = ["Gold", "Silver", "Bronze"]
-    # Delete last flash notice from cookies
     cookies.delete :flash
-    # Find both fighters
     @fighter1 = Fighter.find(params[:fighter1])
     @fighter2 = Fighter.find(params[:fighter2])
-    # Call FightSequence and initialize both fighters
       if @fighter1 == @fighter2
-        flash[:error] = "Both  fighters must be different"
+        flash[:error] = "Both fighters must be different"
         render 'new'
       else
         fight_sequence = FightSequence.new(
@@ -31,7 +25,6 @@ class FightsController < ApplicationController
         @fighter2.accessory_value(params[:weapon2]),
         @fighter2.accessory_value(params[:shield2])
         ).fight
-        # Create new Fight and store the winner and the loser of the FightSequence
         @fight = Fight.new(loser: fight_sequence[:loser], winner: fight_sequence[:winner])
           if @fight.save
             flash[:notice] = fight_sequence[:fight_recap]
--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -21,24 +21,17 @@ class Fighter < ApplicationRecord
 
   def attacks(fighter, weapon, shield)
     if self.lifepoints > 0
-      # Store message that first fighter attacks the other
-      FightSequence.store_message("#{self.name.capitalize} attacks #{fighter.name.capitalize}")
-      # Calculate the damage by adding hitpoints + experience bonus (experience / 10) + random number + weapon power
-      damage_inflicted = self.hitpoints + (self.experience / 10) + FightSequence.randomize_damage + weapon
-      # Store message of the damage inflicted
-      FightSequence.store_message("Damage is #{damage_inflicted}")
-      # Send values to gets_damage method
-      fighter.gets_damage(fighter, damage_inflicted)
+      recap = []
+      recap << "#{self.name.capitalize} attacks #{fighter.name.capitalize}"
+      damage_inflicted = self.hitpoints + (self.experience / 10) + weapon + rand(-5..5)
+      recap << "Damage is #{damage_inflicted}"
+      recap << fighter.gets_damage(damage_inflicted)
     end
   end
 
-  def gets_damage(fighter, damage)
-    # Change fighter's lifepoints depending on damage value
-    fighter.lifepoints -= damage
-    # Store message
-    FightSequence.store_message("#{fighter.name.capitalize} has #{fighter.lifepoints} lifepoints left")
-    # Return fighter's lifepoints
-    fighter.lifepoints
+  def gets_damage(damage)
+    self.lifepoints -= damage
+    inflicted_damage_message = "#{self.name.capitalize} has #{self.lifepoints} lifepoints left"
   end
 
   def accessory_value(accessory)
